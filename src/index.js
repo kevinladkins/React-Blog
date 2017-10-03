6733ec9694bd523fac7938a4415755d6b9c5fd16
github username <kevinladkins@gmail.com>
2017-10-03 13:29:55 -0400

    Add Switch and new routes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(promise )(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 
@@ -16,7 +18,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path='/' component={PostsIndex} />
+        <Switch>
+          <Route path='/posts/new' component={PostsNew} />
+          <Route path='/posts/:id' component={PostsShow} />
+          <Route path='/' component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
